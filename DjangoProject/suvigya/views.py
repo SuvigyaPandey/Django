624c8a0d5b3b192a35a1c85465ba4904c2e4a13f
SuvigyaPandey <89591798+SuvigyaPandey@users.noreply.github.com>
2022-12-23 16:42:11 +0530

    Template Integration

--- DjangoProject/suvigya/views.py
+++ DjangoProject/suvigya/views.py
@@ -6,7 +6,8 @@ def index(request):
     #return HttpResponse("This is HomePage")
     
 def about(request):
-    return HttpResponse("This is AboutPage")
+    return render(request,'about.html')
+    #return HttpResponse("This is AboutPage")
 
 def services(request):
     return HttpResponse("This is ServicesPage")
