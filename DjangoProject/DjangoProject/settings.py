a9946f0d24b95126e5a42b428a2a7b704c7f6678
SuvigyaPandey <89591798+SuvigyaPandey@users.noreply.github.com>
2022-12-24 18:03:08 +0530

    created database

--- DjangoProject/DjangoProject/settings.py
+++ DjangoProject/DjangoProject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "suvigya.apps.SuvigyaConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- DjangoProject/suvigya/views.py
+++ DjangoProject/suvigya/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
+from datetime import datetime
+from suvigya.models import Contact
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -13,4 +14,10 @@ def services(request):
     return HttpResponse("This is ServicesPage")
 
 def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        contact=Contact(name=name,email=email,phone=phone)
+        contact.save()
     return render(request,'contact.html')
